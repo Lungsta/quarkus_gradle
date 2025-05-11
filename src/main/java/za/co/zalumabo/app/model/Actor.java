1a1283f09d69419162c3f338fd16260384903993
Lungelo <lungsta3@gmail.com>
2025-05-11 20:09:55 +0200

    Added the model.

---
+++ src/main/java/za/co/zalumabo/app/model/Actor.java
@@ -0,0 +1,76 @@
+package za.co.zalumabo.app.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "actor", schema = "sakila")
+public class Actor {
+    @Id
+    @Column(name = "actor_id", nullable = false)
+    private Short id;
+
+    @Size(max = 45)
+    @NotNull
+    @Column(name = "first_name", nullable = false, length = 45)
+    private String firstName;
+
+    @Size(max = 45)
+    @NotNull
+    @Column(name = "last_name", nullable = false, length = 45)
+    private String lastName;
+
+    @NotNull
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "last_update", nullable = false)
+    private Instant lastUpdate;
+
+    @ManyToMany(mappedBy = "actors")
+    private Set<Film> films = new HashSet<>();
+
+    public Short getId() {
+        return id;
+    }
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Instant getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Instant lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public Set<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(Set<Film> films) {
+        this.films = films;
+    }
+}
